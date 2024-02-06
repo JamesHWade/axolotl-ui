d6cef6f9f8d8627ce38393ee366253a55e6fca35
James Wade <jhwade@dow.com>
2024-02-06 15:28:00 -0500

    [feat] simple app framework

---
+++ src/axolotl_ui/app.py
@@ -0,0 +1,116 @@
+from pathlib import Path
+
+import seaborn as sns
+import shinyswatch
+
+from shiny import App, Inputs, Outputs, Session, ui
+from htmltools import HTML
+
+from utils import background_img, question_circle_fill
+
+sns.set_theme()
+
+www_dir = Path(__file__).parent.resolve() / "www"
+
+project_config = ui.accordion_panel(
+    "Project Config",
+    *[
+        ui.input_text(
+            id="project_name",
+            label=ui.tooltip(
+                ui.span("Project Name ", question_circle_fill()), "Additional info"
+            ),
+            placeholder="Awesome Axolotl"
+        ),
+        ui.input_password(
+            id="hf_token",
+            label=ui.tooltip(
+                ui.span("HuggingFace Token ", question_circle_fill()), "T"
+            )
+        ),
+        ui.input_text(
+            id="base_model",
+            label=ui.tooltip(
+                ui.span("Base Model ", question_circle_fill()),
+                "This is the huggingface model that contains *.pt, *.safetensors, or *.bin files",
+            ),
+            placeholder="meta-llama/Llama-2-7b-chat-hf",
+        ),
+        ui.input_select(
+            id="model_type",
+            label=ui.tooltip(
+                ui.span("Model Type ", question_circle_fill()),
+                "If you want to specify the type of model to load, AutoModelForCausalLM is a good choice too.",
+            ),
+            choices=["AutoModelForCausalLM", "LlamaForCausalLM"],
+            selected="AutoModelForCausalLM",
+        ),
+        ui.input_select(
+            id="model_derivation",
+            label=ui.tooltip(
+                ui.span("Model Source ", question_circle_fill()),
+                'Used to identify which the model is based on. Please note that if you set this to mistral, `padding_side` will be set to "left" by default',
+            ),
+            choices=["falcon", "llama", "mistra", "qwen"],
+            selected="llama",
+        ),
+        ui.accordion(
+            ui.accordion_panel(
+                "Advanced Config",
+                ui.input_text(id="test1", label="Test 2", placeholder="placeholder"),
+            ),
+            open=False,
+        ),
+    ]
+)
+
+dataset_config = ui.accordion_panel(
+    "Datasets",
+    *[
+        ui.input_text(
+            id="dataset_path",
+            label=ui.tooltip(
+                ui.span("Dataset Path ", question_circle_fill()), "A list of one or more paths to datasets to finetune the model with. HuggingFace dataset repo | s3://,gs:// path | \"json\" for local dataset, make sure to fill data_files"
+            ),
+            placeholder="vicgalle/alpaca-gpt4"
+        ),
+        ui.input_select(
+            id="dataset_type",
+            label=ui.tooltip(
+                ui.span("Dataset Type ", question_circle_fill()), "The type of prompt to use for training. [alpaca, sharegpt, gpteacher, oasst, reflection]"
+            ),
+            choices=["alpaca", "sharegpt", "gpteacher", "oasst", "reflection"],
+            selected="alpaca"
+        )
+    ]
+)
+
+app_ui = ui.page_fillable(
+    shinyswatch.theme.minty(),
+    ui.layout_sidebar(
+        ui.sidebar(
+            ui.h3("Axolotl Laucher 🚀"),
+            ui.accordion(project_config, id="project_config"),
+            ui.accordion(dataset_config, id="dataset_config", open=False),
+            ui.input_action_button(
+                "create_space",
+                "Create HF Space",
+            ),
+            width=400,
+            class_="opacity-75"
+        ),
+        HTML(background_img(url="https://github.com/OpenAccess-AI-Collective/axolotl/raw/main/image/axolotl.png",
+                            opacity=0.1))
+    )
+)
+
+
+def server(input: Inputs, output: Outputs, session: Session):
+    return ()
+
+
+app = App(
+    app_ui,
+    server,
+    static_assets=str(www_dir),
+)
---
+++ src/axolotl_ui/utils.py
@@ -0,0 +1,48 @@
+from shiny import ui
+
+# https://icons.getbootstrap.com/icons/question-circle-fill/
+def question_circle_fill():
+    ui.HTML(
+    '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-question-circle-fill mb-1" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.496 6.033h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286a.237.237 0 0 0 .241.247zm2.325 6.443c.61 0 1.029-.394 1.029-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94 0 .533.425.927 1.01.927z"/></svg>'
+    )
+
+def background_img(url: str, opacity: float) -> str:
+    """
+    Generate CSS style for setting a somewhat transparent background image.
+    
+    Parameters
+    ----------
+    url : str
+        URL of the image to be used as a background.
+    opacity : float
+        Opacity level of the background image (0.0 to 1.0).
+
+    Returns
+    -------
+    str
+        A CSS style string to set the somewhat transparent background image.
+    """
+    return f"""
+    <style>
+    body, html {{
+        width: 100%;
+        height: 100%;
+        margin: 0;
+        padding: 0;
+    }}
+    body::before {{
+        content: "";
+        position: fixed; /* Cover the entire page */
+        left: 0;
+        right: 0;
+        z-index: -1; /* Ensure the image stays in the background */
+        display: block;
+        background-image: url('{url}');
+        background-size: cover; /* Cover the entire page */
+        background-position: center; /* Center the background image */
+        opacity: {opacity}; /* Set the opacity for the image */
+        width: 100%;
+        height: 100%;
+    }}
+    </style>
+    """
